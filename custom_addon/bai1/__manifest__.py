1ac97540313f19a0a1a40eb0977b04ef5a4b0735
Nguyen Danh Binh Giang <NguyenDanhBinhGiang@gmail.com>
2022-09-30 15:09:56 +0700

    initial commit

---
+++ custom_addon/bai1/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Bai 1",
+    'summary': "A test add-on",
+    'author': "me",
+    'category': 'Administration',
+    'depends': ['sale', 'base', 'website_sale'],
+    'data': [
+        'security/group.xml',
+        'security/ir.model.access.csv',
+        'views/template.xml',
+        'wizard/customer_wizard.xml',
+
+    ],
+}
